5c364d137322514dfe91fc9058a376d9489b055c
Mafe-SL <118926563+Mafe-SL@users.noreply.github.com>
2023-01-13 14:39:54 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi practica con CodeSandBox</h1>
+<div>
+  <h2>Universidad Tecnologica de Durango</h2>
+  <h2>María Fernanda Santillan Ledesma</h2>
+  <a href="https://github.com/Mafe-SL/proyecto-react.git" target="_blank" rel="noopener noreferrer">Repositorio</a>.
+</div>
+`;
